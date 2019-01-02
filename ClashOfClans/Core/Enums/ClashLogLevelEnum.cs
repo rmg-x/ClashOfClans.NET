c112a1c01ab31aa9ff74b24a153deb256c08146f
xrmg412 <example@example.com>
2019-01-01 20:04:28 -0600

    Move Enums to "Core" folder Move "LeagueConstants" to "Leagues" folder

---
+++ ClashOfClans/Core/Enums/ClashLogLevelEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClashOfClans
+{
+    public enum ClashLogLevel
+    {
+        None = 0,
+        Verbose = 1,
+        Information = 2
+    }
+}
