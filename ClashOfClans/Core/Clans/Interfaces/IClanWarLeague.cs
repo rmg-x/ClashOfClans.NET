f5561a9af6aa68826165b9f1bdffd31c776857af
rmg-x <git@rmgx.io>
2022-08-20 09:39:07 -0500

    Add LeagueGroup Add ClanWarLeague

---
+++ ClashOfClans/Core/Clans/Interfaces/IClanWarLeague.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClashOfClans.Core.Clans.Interfaces
+{
+    public interface IClanWarLeague : ICurrentWar
+    {
+        DateTime WarStartTime { get; }
+    }
+}
